4e00cfcc2b998d1749b899ad8592e75883c10ae3
Viktor <viktor@apilab.ru>
2023-01-15 20:48:15 +0300

    add interactive to tab

--- projects/popup/src/app/app.component.ts
+++ projects/popup/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { User, UserService } from "shared/user";
 import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
+import { BrowserApiService } from "./services/browser-api.service";
 
 @UntilDestroy()
 @Component({
@@ -9,12 +10,27 @@ import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   user$ = this.userService.user$;
 
   constructor(
     private userService: UserService,
+    private browserApiService: BrowserApiService,
   ) {
+  }
+
+  ngOnInit(): void {
+    console.log('xxx start');
+
+    this.browserApiService.getActiveTab().then(tab => {
+      console.log('xxx tab', tab);
+
+      return this.browserApiService.executeScriptOnTab(tab.id!, this.readDocumentTitle)
+    }).then(res => {
+       console.log('xxx document title', res);
+    }).catch(err => {
+      console.error('error', err)
+    })
   }
 
   updateUser(user: User): void {
@@ -23,5 +39,10 @@ export class AppComponent {
     ).subscribe(() => {
       console.log('xxx user saved')
     })
+  }
+
+  private readDocumentTitle = (): string | null => {
+    const title = document.title;
+    return title || null;
   }
 }
